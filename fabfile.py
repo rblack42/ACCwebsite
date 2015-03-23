65ddb6582f6bddac5afc62a256e3d76e78d0ae27
Roie Black <rblack@austincc.edu>
2015-03-23 08:36:31 -0500

    startup fabfile mods

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,12 @@
 from fabric.api import *
+import os
+
+def setup():
+    if not os.path.isdir('_venv'):
+        local('virtualenv _venv')
+
+def update_reqs():
+    local('source _venv/bin/activate && pip install -r requirements.txt')
 
 def build():
     local('sphinx-build -b html -d _build/doctrees . _build/html')
